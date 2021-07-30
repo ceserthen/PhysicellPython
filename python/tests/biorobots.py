b64569f66d90811fd83c56b139d15f3426508f6f
Donald Belcher <research@donbelcher.com>
2021-07-30 16:56:04 -0400

    Update biorobots.py

--- python/tests/biorobots.py
+++ python/tests/biorobots.py
@@ -30,9 +30,33 @@ def setup_microenvironment(microenv,settings):
     microenv.decay_rates[director_index] = settings.user_parameters.parameter_dict_value("director_signal_decay")#		parameters.doubles("director_signal_D");  
 
     return microenv
+
 if __name__ == "__main__":
     xml_config_file ="PhysiCell_settings.xml"
     mcrenv = BioFVM.Microenvironment(xml_config_file)
     settings = PhysicellSettings(xml_config_file)
     
+    #setting the up the microenvironment
     mcrenv = setup_microenvironment(mcrenv,settings)
+    voxelsize = 30
+    
+    #setting up the cell container
+    cellcontainer = core.Cell_Container(microenvironment = mcrenv, voxel_size = voxelsize)
+    current_time = 0
+    max_time = 5.0
+    
+    diffusion_dt = 1.0
+    
+    mcrenv.simulate_diffusion_decay(diffusion_dt)
+    #Update all the cells
+    cellcontainer.update_all_cells(current_time, diffusion_dt, diffusion_dt, diffusion_dt)
+    
+    
+    while(current_time < (max_time + diffusion_dt)):
+         #Simulate Diffusion in the microenvironment
+         mcrenv.simulate_diffusion_decay(diffusion_dt)
+         #Update all the cells
+         cellcontainer.update_all_cells(current_time, diffusion_dt, diffusion_dt, diffusion_dt)
+         #Update the current time
+         core.save_MultiCellDS(filename = "test"+str(current_time), microenvironment = mcrenv, cell_container = cellcontainer, current_simulation_time = current_time)
+         current_time += diffusion_dt
